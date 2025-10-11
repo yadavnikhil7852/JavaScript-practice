34da01b4ef616922e2036ed7ae4629be0d46300f
Nikhil Yadav <nikhilyadav52168@gmail.com>
2025-10-11 15:39:35 +0530

    Create Weather-app.js

---
+++ Weather-app.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wheather project</title>
+</head>
+<body>
+    <div class="main">
+        <div class="heading"><h1>Wheather App</h1></div>
+    <input type="text" id="cityInput" placeholder="Enter your city">
+    <button id="getWeather"> Get Weather ⛅</button>
+    
+    
+    <div id="output"></div>
+    <style>
+        .main{
+            display: grid;
+            align-items: center;
+            justify-content: center;
+            justify-content: space-evenly;
+            margin-top: 100px;
+        }
+        #cityInput{
+            margin-top: 2px;
+            margin-bottom: 7px;
+            height: 40px;
+            border-radius: 5px;
+            text-align: center;
+        }
+        #getWeather{
+            color: black;
+            background-color: rgb(105, 237, 105);
+        }
+
+    </style>
+    <script src="Weather-Project.js"></script>
+    </div>
+</body>
+</html>
